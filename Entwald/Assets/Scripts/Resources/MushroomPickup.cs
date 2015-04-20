3be79bdb50cddf689156ab9bd4e197da399cc24c
Kitty Liang <lkitty491@yahoo.com>
2015-04-20 10:17:29 -0700

    Added item pickups for dog and player

---
+++ Entwald/Assets/Scripts/Resources/MushroomPickup.cs
@@ -0,0 +1,29 @@
+﻿// Attach script to mushrooms
+
+using UnityEngine;
+using System.Collections;
+
+public class MushroomPickup : MonoBehaviour {
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerStay(Collider other){
+		if (other.transform.tag == "Player"){
+			Debug.Log("In mushroom collider");
+			// Press "E" to take item
+			if (Input.GetKeyDown(KeyCode.E)){
+				// Add to inventory then remove object from scene
+				// How do I add it to inventory?
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
---
+++ Entwald/Assets/Scripts/Resources/StickPickups.cs
@@ -0,0 +1,28 @@
+﻿// Attach script to sticks
+
+using UnityEngine;
+using System.Collections;
+
+public class StickPickups : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnTriggerStay(Collider other){
+		if (other.transform.tag == "Dog"){
+			// Only take item if dog is close enough
+			if (Vector3.Distance(this.transform.position, other.transform.position)<2.0f){
+				// Add to inventory then remove object from scene
+				// How do I add it to inventory?
+				Destroy (this.gameObject);
+			}
+		}
+	}
+}
