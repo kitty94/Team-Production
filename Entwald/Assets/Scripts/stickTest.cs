e93ab26b11ac7d65e7bbfac0df0edab817eb916b
jkim1987 <buhssuht@hotmail.com>
2015-04-24 08:17:58 -0700

    Durability
    Script by Jim

---
+++ Entwald/Assets/Scripts/stickTest.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class stickTest : MonoBehaviour {
+
+	public float stickdurability = 100;
+	private float currentDurability;
+	private float durabilityDamage = 10;
+
+	public KeyCode stickAttack;
+
+	public GameObject TestStick;
+
+	public GameObject OwlTest;
+	
+
+	// Use this for initialization
+	void Start () {
+		//OwlTest.gameObject = new durabilityDamage;
+
+		currentDurability = stickdurability;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (stickAttack)) {
+						TestStick.transform.Rotate (0, 90, 0);
+				}
+
+
+		if (currentDurability == 0) {
+			
+			Destroy(this.gameObject);
+			
+		}
+		
+	}
+	
+	//void OnTriggerEnter(Collision stickHit){
+	void OnTriggerEnter(Collider stickHit){
+		if (stickHit.gameObject.tag == "Owl") {
+						//currentDurability = stickdurability - durabilityDamage;
+			currentDurability -= durabilityDamage;
+				}
+		Debug.Log (currentDurability);
+		
+	}
+}
