cb4ef136de12569080a7c0ba8d7095adf258eafe
parargonzs <parargonzs@stu.aii.edu>
2015-04-24 08:41:06 -0700

    Inv
    added stufffffy for inv

---
+++ Entwald/Assets/Scripts/Equpping.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Equpping : MonoBehaviour {
+	public void onStick()
+	{
+		Debug.Log ("Using Stick");
+	}
+
+	public void onMushroom()
+	{
+		Debug.Log ("Using Mushroom");
+	}
+
+	public void onFlashLight()
+	{
+		Debug.Log ("Using Flashlight");
+	}
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Entwald/Assets/Scripts/OpenInv.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class OpenInv : MonoBehaviour {
+	public Animator InventoryAnim;
+	public GameObject Slots;
+
+	// Use this for initialization
+	void Start () {
+		Slots.SetActive(false);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.LeftShift))
+		{
+			Slots.SetActive(true);
+		}
+		else if(Input.GetKeyUp(KeyCode.LeftShift))
+		{
+			Slots.SetActive(false);
+		}
+	}
+}
